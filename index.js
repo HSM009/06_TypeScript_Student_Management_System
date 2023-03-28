8e1270b869e079dee62021738a69df64fc128328
HSM009 <hoseinsirat@gmail.com>
2023-03-28 12:44:18 +0500

    Commit mainMenu

--- index.js
+++ index.js
@@ -1,15 +1,74 @@
 #! /usr/bin/env node
 console.clear();
-export {};
 //------------------------------------------------------------------------------
 //IMPORTS HERE
 //------------------------------------------------------------------------------
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
 //------------------------------------------------------------------------------
 //CLASSES/CONSTRUCTORS/OOPs HERE
 //------------------------------------------------------------------------------
+const stopTime = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3500);
+    });
+};
+const preDefinedMain = {
+    showClass: "showClass",
+    addClass: "addClass",
+    Bye: "bye"
+};
 //------------------------------------------------------------------------------
 //FUNCTIONS HERE
 //------------------------------------------------------------------------------
+async function welcomeFunc(welcomeMessage) {
+    let rainbowTitle = chalkAnimation.neon(chalk.blueBright("Welcome To " + welcomeMessage + " App!\n\nCoded By Hosein Sirat Mohammad\n"));
+    await stopTime();
+    rainbowTitle.stop();
+}
+async function mainMenuFunc() {
+    await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            choices: [
+                {
+                    name: "Show all classes",
+                    value: preDefinedMain.showClass
+                },
+                {
+                    name: "Add a class",
+                    value: preDefinedMain.addClass
+                },
+                {
+                    name: "Bye",
+                    value: preDefinedMain.Bye
+                }
+            ]
+        }
+    ]).then((selected) => {
+        if (selected.option == preDefinedMain.showClass) {
+            showClassFunc();
+        }
+        else if (selected.option == preDefinedMain.addClass) {
+            addClassFunc();
+        }
+        else {
+            console.log(preDefinedMain.Bye);
+        }
+    });
+}
+;
+async function showClassFunc() {
+}
+;
+async function addClassFunc() {
+}
+;
 //------------------------------------------------------------------------------
-//MAIN
+//MAIN HERE
 //------------------------------------------------------------------------------
+let appName = "Student Management System";
+await welcomeFunc(appName);
+await mainMenuFunc();
--- index.ts
+++ index.ts
@@ -1,18 +1,93 @@
 #! /usr/bin/env node
 
 console.clear();
+
 //------------------------------------------------------------------------------
 //IMPORTS HERE
 //------------------------------------------------------------------------------
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
 
 //------------------------------------------------------------------------------
 //CLASSES/CONSTRUCTORS/OOPs HERE
 //------------------------------------------------------------------------------
 
+const stopTime = ()=>{
+    return new Promise((res:any)=>{
+        setTimeout(res,3500);
+    })
+}
+
+const preDefinedMain = {
+    showClass:  "showClass",
+    addClass:   "addClass",
+    Bye:        "bye"
+};
+
 //------------------------------------------------------------------------------
 //FUNCTIONS HERE
 //------------------------------------------------------------------------------
 
+async function welcomeFunc(welcomeMessage:string ) {
+    let rainbowTitle = chalkAnimation.neon(chalk.blueBright("Welcome To "+ welcomeMessage +" App!\n\nCoded By Hosein Sirat Mohammad\n"));
+    await stopTime();
+    rainbowTitle.stop();
+}
+
+async function mainMenuFunc(){
+
+    await inquirer.prompt([
+        {
+            type:   'list',
+            name:   'option',
+            choices:[
+                {
+                    name:   "Show all classes",
+                    value:  preDefinedMain.showClass
+                },
+                {
+                    name:   "Add a class",
+                    value:  preDefinedMain.addClass
+                },
+                {
+                    name:   "Bye",
+                    value:  preDefinedMain.Bye
+                }   
+            ]   
+        }
+        
+    ]).then((selected) => {
+        if (selected.option == preDefinedMain.showClass)
+        {
+            showClassFunc();
+        }
+        else if(selected.option == preDefinedMain.addClass)
+        {
+            addClassFunc();
+        }
+        else
+        {
+            console.log(preDefinedMain.Bye);
+        }
+    });
+
+};
+
+async function showClassFunc() {
+    
+};
+
+async function addClassFunc() {
+    
+};
+
 //------------------------------------------------------------------------------
-//MAIN
+//MAIN HERE
 //------------------------------------------------------------------------------
+
+let appName:string = "Student Management System";
+await welcomeFunc(appName);
+
+await mainMenuFunc();
